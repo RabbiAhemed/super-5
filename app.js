c1c0125108cd63044260194287e64a083f116484
Rabbi Ahmed Limon <rabbiahemed>
2022-08-22 15:02:31 +0600

    buttons added

--- app.js
+++ app.js
@@ -23,4 +23,39 @@ function addToList(player) {
   showList(playersList);
 }
 
+/* BUDGET PART */
+
 // const NumberOfPlayers = playersList.length;
+const perPlayerInput = document.getElementById("per-player").value;
+const perPlayerCostFloat = parseFloat(perPlayerInput);
+
+const managerCostInput = document.getElementById("manager-cost").value;
+const managerCostFloat = parseFloat(managerCostInput);
+console.log(managerCostFloat);
+
+const coachCostInput = document.getElementById("coach-cost").value;
+const coachCostFloat = parseFloat(coachCostInput);
+
+// calculate button
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  console.log(perPlayerCostFloat);
+});
+
+// calculate Total Expenses
+// document.getElementById("total-btn").addEventListener("click", function () {
+//   cons;
+//   function calculateCost(cost1, cost2, cost3) {
+//     finalCost = cost1 + cost2 + cost3;
+//     return finalCost;
+//     console.log(finalCost);
+//   }
+//   console.log(coachCostFloat);
+//   calculateCost(managerCostFloat, coachCostFloat);
+// });
+
+// // calculate function
+// function calculateCost(cost1, cost2, cost3) {
+//   finalCost = cost1 + cost2 + cost3;
+//   return finalCost;
+//   console.log(finalCost);
+// }
